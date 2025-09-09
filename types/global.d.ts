805bb5620b9a3e71ffeaf2a3738391b9ae6ce063
Clayton <dropifytoken@gmail.com>
2025-09-09 13:48:57 -0700

    EdIT

---
+++ types/global.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="react" />
+/// <reference types="react-dom" />
+/// <reference types="node" />
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    // Allow any data attributes
+    [key: `data-${string}`]: any;
+  }
+}
+
+declare module 'react-dom/client' {
+  export * from 'react-dom';
+}
+
+// Global type extensions
+declare global {
+  interface Window {
+    Telegram?: any;
+    ethereum?: any;
+  }
+}
+
+export {};
